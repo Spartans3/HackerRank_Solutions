a39af670791d96c6b2e46c7531b7e2946f8145e0
Atacan Okay <atacanokay@gmail.com>
2022-09-16 20:03:27 +0300

    JimAndTheOrders

---
+++ Problem Solving (Algorithms)/JimAndTheOrders.java
@@ -0,0 +1,92 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+	//https://www.hackerrank.com/challenges/jim-and-the-orders
+    /*
+     * Complete the 'jimOrders' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts 2D_INTEGER_ARRAY orders as parameter.
+     */
+
+    public static List<Integer> jimOrders(List<List<Integer>> orders) {
+    // Write your code here
+    	List<Integer> orderList = new ArrayList<Integer>();
+    	Iterator<List<Integer>> orderIterator = orders.iterator();
+    	Map<Integer,Integer> map = new LinkedHashMap<Integer, Integer>();
+    	int iteratorNum = 1;
+    	while(orderIterator.hasNext()) {
+    		List<Integer> listPoint = orderIterator.next();
+//    		orderList.add(listPoint.get(0) + listPoint.get(1));  
+    		map.put(iteratorNum, listPoint.get(0) + listPoint.get(1)); 
+    		iteratorNum++;
+    	}
+    	
+    	map = sortByValue(map);
+    	
+    	
+    	orderList.addAll(map.keySet());
+    	
+    	return orderList;
+    }
+    
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Entry<K, V>> list = new ArrayList<>(map.entrySet());
+        list.sort(Entry.comparingByValue());
+
+        Map<K, V> result = new LinkedHashMap<>();
+        for (Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+
+        return result;
+    }
+
+}
+
+public class JimAndTheOrders {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> orders = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                orders.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        List<Integer> result = Result.jimOrders(orders);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
